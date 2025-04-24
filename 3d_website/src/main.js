00ee3aa5bcd66c230346c43748ad931ccb7ba14c
yodanisesutantio <yodanise66@gmail.com>
2025-04-24 14:32:18 +0700

    Installed and Imported Three JS from npm, also added a mesh and camera to a scene then render the scene

--- 3d_website/src/main.js
+++ 3d_website/src/main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
 
-setupCounter(document.querySelector('#counter'))
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+scene.add(cubeMesh);
+
+// console.log(scene);
+console.log(window.innerWidth, window.innerHeight);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  30
+);
+
+camera.position.z = 5;
+// scene.add(camera);
+
+// Initialize renderer
+const canvas = document.querySelector("canvas.threejs");
+// console.log(canvas);
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
