e2a1b8a07632d18bf2061d355818c9bb6905caad
yodanisesutantio <yodanise66@gmail.com>
2025-04-24 18:32:22 +0700

    imported OrbitControls and implemented renderloop

--- 3d_website/src/script.js
+++ 3d_website/src/script.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// console.log(OrbitControls);
 
 const scene = new THREE.Scene();
 
@@ -10,7 +13,7 @@ const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cubeMesh);
 
 // console.log(scene);
-console.log(window.innerWidth, window.innerHeight);
+// console.log(window.innerWidth, window.innerHeight);
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -26,6 +29,17 @@ camera.position.z = 5;
 const canvas = document.querySelector("canvas.threejs");
 // console.log(canvas);
 const renderer = new THREE.WebGLRenderer({ canvas });
+// Initialize the controls
+const controls = new OrbitControls(camera, canvas);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+const renderloop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
